06cd291b5577be92a0ffb5c32a12a8bd4263551d
nicey0 <nicey_pm@protonmail.com>
2020-07-21 17:47:15 -0300

    repeating code? CRINGE

--- imageboard/boards.py
+++ imageboard/boards.py
@@ -5,17 +5,20 @@ from .db import pdb, Post
 bp = Blueprint('boards', __name__)
 PAGE_SIZE = 10
 
+def board_addpost(method, form, pdb):
+    if method == 'POST':
+        pdb.session.add(
+            Post(body=form["body"])
+        )
+        pdb.session.commit()
+
 @bp.route('/')
 def index():
     return "Index"
 
 @bp.route('/<board>/', methods=['GET', 'POST'])
 def board_paged(board):
-    if request.method == 'POST':
-        body = request.form["body"]
-        post = Post(body=body)
-        pdb.session.add(post)
-        pdb.session.commit()
+    board_addpost(request.method, request.form, pdb)
     try:
         page = int((lambda x: x if x is not None else 0)(request.args.get('page', None)))
     except ValueError:
@@ -25,9 +28,5 @@ def board_paged(board):
 
 @bp.route('/<board>/catalog', methods=['GET', 'POST'])
 def board_catalog(board):
-    if request.method == 'POST':
-        body = request.form["body"]
-        post = Post(body=body)
-        pdb.session.add(post)
-        pdb.session.commit()
+    board_addpost(request.method, request.form, pdb)
     return jsonify([str(p) for p in Post.query.all()])
